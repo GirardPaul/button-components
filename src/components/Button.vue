18459e539c1c9db4d78bb9716fa21bcb18f57a58
Whyzen <girard.paul39@gmail.com>
2023-03-28 15:30:11 +0200

    [Add] project files

---
+++ src/components/Button.vue
@@ -0,0 +1,165 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  variant: String,
+  size: {
+    type: String,
+    default: "md",
+  },
+  disableShadow: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  startIcon: String,
+  endIcon: String,
+  color: {
+    type: String,
+    default: "default",
+  },
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    class="btn"
+    :class="[
+      variant,
+      size,
+      color,
+      disableShadow ? 'disableShadow' : '',
+      disabled ? 'disabled' : '',
+    ]"
+  >
+    <template v-if="startIcon">
+      <div class="flex space-between align-center">
+        <span class="material-icons mr-9">{{ startIcon }}</span>
+        <span><slot></slot></span>
+      </div>
+    </template>
+    <template v-else-if="endIcon">
+      <div class="flex space-between align-center">
+        <span><slot></slot></span>
+        <span class="material-icons ml-9">{{ endIcon }}</span>
+      </div>
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
+  </button>
+</template>
+
+<style scoped>
+.mr-9 {
+  margin-right: 9px;
+}
+.ml-9 {
+  margin-left: 9px;
+}
+.flex {
+  display: flex;
+}
+.space-between {
+  justify-content: space-between;
+}
+
+.align-center {
+  align-items: center;
+}
+.sm {
+  height: 32px;
+  padding: 6px 12px;
+}
+.md {
+  height: 36px;
+  padding: 8px 16px;
+}
+.lg {
+  height: 42px;
+  padding: 11px 22px;
+}
+.btn {
+  background-color: #e0e0e0;
+  border-radius: 6px;
+  font-size: 14px;
+  color: #3f3f3f;
+  font-weight: 500;
+  width: auto;
+  border: none;
+  cursor: pointer;
+  box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+}
+
+.outline {
+  border: 1px solid #3d5afe;
+}
+
+.outline:hover,
+.outline:focus,
+.text:hover,
+.text:focus {
+  background: rgba(41, 98, 255, 0.1) !important;
+}
+
+.btn:hover,
+.btn:focus {
+  background-color: #aeaeae;
+}
+
+.text,
+.outline {
+  color: #3d5afe !important;
+  background: transparent !important;
+  box-shadow: none;
+}
+
+.disableShadow,
+.disabled {
+  box-shadow: none !important;
+}
+
+.disabled {
+  color: #9e9e9e !important;
+  cursor: default;
+}
+
+.disabled:hover,
+.disabled:focus {
+  background-color: #e0e0e0 !important;
+}
+
+.text.disabled:hover,
+.text.disabled:focus {
+  background-color: transparent !important;
+}
+
+.btn.primary {
+  background-color: #2962ff;
+  color: #fff;
+}
+.btn.primary:hover,
+.btn.primary:focus {
+  background-color: #0039cb;
+}
+.btn.secondary {
+  background-color: #455a64;
+  color: #fff;
+}
+.btn.secondary:hover,
+.btn.secondary:focus {
+  background-color: #1c313a;
+}
+.btn.danger {
+  background-color: #d32f2f;
+  color: #fff;
+}
+.btn.danger:hover,
+.btn.danger:focus {
+  background-color: #9a0007;
+}
+</style>
---
+++ src/layout/Home.vue
@@ -0,0 +1,154 @@
+<script setup>
+import Sidebar from "../components/Sidebar.vue";
+import Button from "../components/Button.vue";
+import { ref } from "vue";
+
+const btnDefault = ref("<Button />");
+const btnOutline = ref('<Button variant="outline" />');
+const btnText = ref('<Button variant="text" />');
+const btnDisableShadow = ref("<Button disableShadow />");
+const btnDisabled = ref("<Button disabled />");
+const btnVariantDisabled = ref('<Button variant="text" disabled />');
+const btnStartIcon = ref('<Button startIcon="local_grocery_store" />');
+const btnEndIcon = ref('<Button endIcon="local_grocery_store" />');
+const btnSm = ref('<Button size="sm" />');
+const btnMd = ref('<Button size="md" />');
+const btnLg = ref('<Button size="lg" />');
+const btnColorDefault = ref('<Button color="default" />');
+const btnColorPrimary = ref('<Button color="primary" />');
+const btnColorSecondary = ref('<Button color="secondary" />');
+const btnColorDanger = ref('<Button color="danger" />');
+</script>
+
+<template>
+  <div class="flex">
+    <Sidebar />
+    <div class="full flex flex-column buttons-container">
+      <h3>Buttons</h3>
+      <div class="flex flex-wrap">
+        <div class="col">
+          <p>{{ btnDefault }}</p>
+          <Button>Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnOutline }}</p>
+          <Button variant="outline">Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnText }}</p>
+          <Button variant="text">Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnDisableShadow }}</p>
+          <Button disableShadow>Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnDisabled }}</p>
+          <Button disabled>Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnVariantDisabled }}</p>
+          <Button variant="text" disabled>Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnStartIcon }}</p>
+          <Button startIcon="local_grocery_store">Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnEndIcon }}</p>
+          <Button endIcon="local_grocery_store">Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnSm }}</p>
+          <Button size="sm">Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnMd }}</p>
+          <Button size="md">Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnLg }}</p>
+          <Button size="lg">Default</Button>
+        </div>
+      </div>
+      <div class="flex flex-wrap mt-45">
+        <div class="col">
+          <p>{{ btnColorDefault }}</p>
+          <Button color="default">Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnColorPrimary }}</p>
+          <Button color="primary">Default</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnColorSecondary }}</p>
+          <Button color="secondary">Secondary</Button>
+        </div>
+        <div class="col">
+          <p>{{ btnColorDanger }}</p>
+          <Button color="danger">Danger</Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+p {
+  font-size: 12px;
+  font-weight: 400;
+}
+.mt-45 {
+  margin-top: 45px;
+}
+.full {
+  width: 100%;
+}
+.col {
+  flex: 0 0 auto;
+}
+.buttons-container {
+  padding: 50px 82px;
+}
+h3 {
+  font-weight: 500;
+  font-size: 24px;
+}
+.flex {
+  display: flex;
+}
+.flex-wrap {
+  flex-wrap: wrap;
+}
+.flex-column {
+  flex-direction: column;
+}
+
+@media screen and (min-width: 992px) {
+  .col {
+    width: 25% !important;
+  }
+}
+@media screen and (max-width: 991px) {
+  .col {
+    width: 50% !important;
+  }
+}
+@media screen and (max-width: 767px) {
+  .col {
+    width: 100% !important;
+  }
+}
+</style>
